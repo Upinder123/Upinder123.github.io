3f29127c506c14ba79fccedd91e77878ead4797e
Upinderjit Singh <upinderjits@gmail.com>
2021-07-27 15:02:54 +0530

    blog

---
+++ precache-manifest.e8b1606140ed0e4e62134dcf2a7ba83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e761f5344113e41b2286cea9a94676e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca6ccb3543492b464c3",
+    "url": "/static/css/main.33e61469.chunk.css"
+  },
+  {
+    "revision": "b0407cd4ebaa7efc702e",
+    "url": "/static/js/2.c44a4828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c44a4828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca6ccb3543492b464c3",
+    "url": "/static/js/main.a6d96627.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
