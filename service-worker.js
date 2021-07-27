e765475b66c9f364ffc13425e4315a4f0297c139
Upinderjit Singh <upinderjits@gmail.com>
2021-07-27 22:41:15 +0530

    404 bug resolved

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c09e862b28e3c5d41708e958fe89ee6.js"
+  "/precache-manifest.f10b78d0dc1f8019c05c2346b33216df.js"
 );
 
 self.addEventListener('message', (event) => {
